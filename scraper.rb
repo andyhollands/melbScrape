5415f65e7dfee67b756fcfe0f8c7b15996d1eb59
Rob Tedge <rjtedge@users.noreply.github.com>
2017-08-30 14:16:57 +1000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,121 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'mechanize'
+require 'scraperwiki'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+agent = Mechanize.new
+urlbase = 'https://www.domain.com.au/sale/?suburb=brunswick-vic-3056,brunswick-west-vic-3055,coburg-north-vic-3058,coburg-vic-3058,pascoe-vale-south-vic-3044&ptype=house,villa,town-house,semi-detached,terrace,duplex,new-home-designs,new-house-land&bedrooms=2-any&price=any-900000'
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+  p "page 1"
+  page = agent.get(urlbase)
+  
+  @propertyurls = Array.new
+  @lats = Array.new
+  @longs = Array.new
+  page.search('.listing-result__standard-standard , .listing-result__standard-premium, .listing-result__standard-pp').each do |li|
+    @propertyurls << li.at('.listing-result__left > a').attributes['href'].value
+    @lats << li.at('a div meta:nth-child(1)').attributes['content'].value
+    @longs << li.at('a div meta:nth-child(2)').attributes['content'].value
+  end
+  i = 0
+  @propertyurls.each do |li|
+    page2 = agent.get(li)
+    address = page2.at('div.left-wrap > h1').inner_text.strip
+    beds = page2.at('.listing-features.alt > span:nth-child(1) > span.copy > em').inner_text.strip
+    baths = page2.at('.listing-features.alt > span:nth-child(2) > span.copy > em').inner_text.strip
+    cars = page2.at('.listing-features.alt > span:nth-child(3) > span.copy > em').inner_text.strip
+    agents = page2.at('#propertyStoryPartial > div > div > div > a:nth-child(3)').inner_text.strip
+    pricetest = page2.at('#main > div > header > div > div.left-wrap > span').inner_text.strip.to_s
+    low = ''
+    high = ''
+    lat = @lats[i]
+    long = @longs[i]
+    if pricetest.start_with? '$'
+      price = pricetest.delete! '$ ,'
+      if price.include? "-"
+          low = price.partition('-').first
+          high = price.partition('-').last
+      else
+          high = price
+      end
+    elsif page2.at('div:nth-child(1) > p.statement-of-information__data-point-value')
+         low = page2.at('div:nth-child(1) > p.statement-of-information__data-point-value').inner_text.strip.delete! '$ ,'
+         low = low.partition('-').first
+         high = page2.at('div:nth-child(1) > p.statement-of-information__data-point-value').inner_text.strip.delete! '$ ,'
+         high = high.partition('-').last
+    end
+     house = {
+       address: address,
+       beds: beds,
+       baths: baths,
+       cars: cars,
+       agent: agents,
+       lowprice: low,
+       highprice: high,
+       lat: lat,
+       long: long,
+       link: li
+     }   
+      p house 
+      ScraperWiki.save_sqlite([:address], house)
+     i += 1
+  end
+     
+     pageurl = 2
+    
+ while pageurl <= 5 do
+    url = urlbase + "&page=" +  pageurl.to_s
+    p "page " +  pageurl.to_s
+    page = agent.get(url)
+    @propertyurls.clear
+    @lats.clear
+    @longs.clear
+    
+    page.search('.listing-result__standard-standard , .listing-result__standard-premium, .listing-result__standard-pp').each do |li|
+    @propertyurls << li.at('.listing-result__left > a').attributes['href'].value
+    @lats << li.at('a div meta:nth-child(1)').attributes['content'].value
+    @longs << li.at('a div meta:nth-child(2)').attributes['content'].value
+  end
+  i = 0
+  @propertyurls.each do |li|
+    page2 = agent.get(li)
+    address = page2.at('div.left-wrap > h1').inner_text.strip
+    beds = page2.at('.listing-features.alt > span:nth-child(1) > span.copy > em').inner_text.strip
+    baths = page2.at('.listing-features.alt > span:nth-child(2) > span.copy > em').inner_text.strip
+    cars = page2.at('.listing-features.alt > span:nth-child(3) > span.copy > em').inner_text.strip
+    agents = page2.at('#propertyStoryPartial > div > div > div > a:nth-child(3)').inner_text.strip
+    pricetest = page2.at('#main > div > header > div > div.left-wrap > span').inner_text.strip.to_s
+    low = ''
+    high = ''
+    lat = @lats[i]
+    long = @longs[i]
+    if pricetest.start_with? '$'
+      price = pricetest.delete! '$ ,'
+      if price.include? "-"
+          low = price.partition('-').first
+          high = price.partition('-').last
+      else
+          high = price
+      end
+    elsif page2.at('div:nth-child(1) > p.statement-of-information__data-point-value')
+         low = page2.at('div:nth-child(1) > p.statement-of-information__data-point-value').inner_text.strip.delete! '$ ,'
+         low = low.partition('-').first
+         high = page2.at('div:nth-child(1) > p.statement-of-information__data-point-value').inner_text.strip.delete! '$ ,'
+         high = high.partition('-').last
+    end
+     house = {
+       address: address,
+       beds: beds,
+       baths: baths,
+       cars: cars,
+       agent: agents,
+       lowprice: low,
+       highprice: high,
+       lat: lat,
+       long: long,
+       link: li
+     }   
+      p house 
+     ScraperWiki.save_sqlite([:address], house)
+     i += 1
+  end
+  pageurl += 1
+  end
