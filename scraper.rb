fc8f570a35deda9c1b4072495f6e7f76bd39d955
andyhollands <andy@andyhollands.co>
2020-03-02 13:28:47 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,5 @@
 require 'mechanize'
 require 'scraperwiki'
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 agent = Mechanize.new{|a| a.ssl_version, a.verify_mode = 'SSLv3', OpenSSL::SSL::VERIFY_NONE}
 agent.user_agent_alias = 'Mac Safari'
