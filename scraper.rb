396893d9b249912ee01d504f10e5739879e32e7a
andyhollands <andy@andyhollands.co>
2020-03-02 13:22:55 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,7 @@
 require 'mechanize'
 require 'scraperwiki'
 
-agent = Mechanize.new{|agent|agent.user_agent_alias = 'Windows Chrome'|a| a.ssl_version, a.verify_mode = 'SSLv3', OpenSSL::SSL::VERIFY_NONE}
+agent = Mechanize.new{|agent|agent.user_agent_alias = 'Windows Chrome',|a| a.ssl_version, a.verify_mode = 'SSLv3', OpenSSL::SSL::VERIFY_NONE}
 urlbase = 'https://www.domain.com.au/sale/?suburb=melbourne-vic-3000&ptype='
 filter = '&price=any-1500000&sort=dateupdated-desc'
 
