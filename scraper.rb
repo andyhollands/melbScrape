3e9bc8f55a9cd160618995eb69da7b92a1880afb
andyhollands <andy@andyhollands.co>
2020-03-02 13:48:26 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,16 +1,5 @@
 require 'mechanize'
 require 'scraperwiki'
-ScraperWiki.save_sqlite([:address], { address:"address",
-       propertytype:"proptype",
-       beds:"beds",       baths:"baths",
-       cars:"cars",
-       agent:"agents",
-       lowprice:"low",
-       highprice:"high",
-       lat:"lat",
-       long:"long",
-       link:"li"})
-
 Mechanize.new{|a| a.ssl_version, a.verify_mode = 'SSLv3', OpenSSL::SSL::VERIFY_NONE};
 agent.user_agent_alias = 'Mac Safari'
 urlbase = 'https://www.domain.com.au/sale/?suburb=melbourne-vic-3000&ptype='
