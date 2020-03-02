0c38c430b0a60f4af562833a5005fd5fe426a369
andyhollands <andy@andyhollands.co>
2020-03-02 13:32:33 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,7 @@
 require 'mechanize'
 require 'scraperwiki'
 
-Mechanize.new{|a| a.ssl_version, OpenSSL::SSL::VERIFY_NONE}
+Mechanize.new{|a| a.ssl_version, a.verify_mode = 'SSLv3', OpenSSL::SSL::VERIFY_NONE};
 agent.user_agent_alias = 'Mac Safari'
 urlbase = 'https://www.domain.com.au/sale/?suburb=melbourne-vic-3000&ptype='
 filter = '&price=any-1500000&sort=dateupdated-desc'
